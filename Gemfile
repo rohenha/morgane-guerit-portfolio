762d45005a5242d701f4c4221cc167147382bacb
Romain Breton <romain.breton.pro@gmail.com>
2020-04-20 16:10:12 +0200

    fix content

--- Gemfile
+++ Gemfile
@@ -1,18 +1,19 @@
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 4.2.0'
-gem 'middleman-autoprefixer'
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
-gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
-gem "middleman-livereload"
-gem "middleman-sprockets"
-gem 'bootstrap'
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem "middleman-livereload", "~> 3.4.3"
+gem 'middleman-s3_sync'
+gem 'builder', '~> 3.0'
+gem 'mime-types'
 gem 'sass', '~> 3.4'
 gem 'sass-globbing', '~> 1.1'
 gem 'middleman-gdpr'
-gem 'mini_racer' # faster JS compiles
-gem 'mime-types'
-gem 'rspec'
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
 gem 'nokogiri'
-gem "middleman-minify-html"
+gem 'rspec'
 gem 'sprockets', '~> 3.0'
+gem 'bootstrap'
+gem "middleman-minify-html"
--- source/assets/javascripts/_app.js
+++ source/assets/javascripts/_app.js
@@ -1,5 +1,5 @@
 /*  eslint no-param-reassign: 0  */
-
+/* global lightGallery */
 window.morganeGuerit.App = {
     init: function () {
         'use strict';
@@ -7,6 +7,7 @@ window.morganeGuerit.App = {
         this.portfolio = window.morganeGuerit.Portfolio.init(this.toggleElements.bind(this));
         this.sliders = window.morganeGuerit.Sliders.init('.js-slider', '.js-slide');
         this.article = window.morganeGuerit.Article.init();
+        this.setLightbox();
     },
 
     invoke: function () {
@@ -14,6 +15,22 @@ window.morganeGuerit.App = {
         return {
             init: this.init.bind(this)
         };
+    },
+
+    setLightbox: function () {
+        'use strict';
+        var lightbox = document.querySelectorAll('.js-lightbox'),
+            i = 0,
+            length = lightbox.length;
+
+        for (i; i < length; i += 1) {
+            lightGallery(lightbox[i], {
+                download: false,
+                galleryId: i,
+                mode: 'lg-slide',
+                selector: 'li'
+            });
+        }
     },
 
     toggleElements: function (value, first, second) {
